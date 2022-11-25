ef1a8e5f7aedebece9f18798727cbc01c2f75ea2
ItzelCabrera <ihernandezr1601@alumno.ipn.mx>
2022-11-24 23:09:32 -0600

    Interfaz para consultas a,e,f y g

--- Proj_Frag_App/Form1.Designer.cs
+++ Proj_Frag_App/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Proj_Frag_App
 {
-    partial class Form1
+    partial class frmPRINCIPAL
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -35,7 +35,35 @@ namespace Proj_Frag_App
             this.rbtn4 = new System.Windows.Forms.RadioButton();
             this.btnUPDATE = new System.Windows.Forms.Button();
             this.btnREAD = new System.Windows.Forms.Button();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlCONSULTAS = new System.Windows.Forms.Panel();
+            this.pnlConF = new System.Windows.Forms.Panel();
+            this.txtCF1 = new System.Windows.Forms.TextBox();
+            this.lblCF1 = new System.Windows.Forms.Label();
+            this.pnlConE = new System.Windows.Forms.Panel();
+            this.lblCE2 = new System.Windows.Forms.Label();
+            this.txtCE1 = new System.Windows.Forms.TextBox();
+            this.lblCE1 = new System.Windows.Forms.Label();
+            this.btnGO = new System.Windows.Forms.Button();
+            this.dataGV = new System.Windows.Forms.DataGridView();
+            this.pnlConG = new System.Windows.Forms.Panel();
+            this.txtCG2 = new System.Windows.Forms.TextBox();
+            this.lblCG2 = new System.Windows.Forms.Label();
+            this.txtCG1 = new System.Windows.Forms.TextBox();
+            this.lblCG1 = new System.Windows.Forms.Label();
+            this.lblCE3 = new System.Windows.Forms.Label();
+            this.txtCE2 = new System.Windows.Forms.TextBox();
+            this.txtCE3 = new System.Windows.Forms.TextBox();
+            this.txtCF2 = new System.Windows.Forms.TextBox();
+            this.lblCF2 = new System.Windows.Forms.Label();
+            this.pnlConA = new System.Windows.Forms.Panel();
+            this.txtCA1 = new System.Windows.Forms.TextBox();
+            this.lblCA1 = new System.Windows.Forms.Label();
+            this.pnlCONSULTAS.SuspendLayout();
+            this.pnlConF.SuspendLayout();
+            this.pnlConE.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV)).BeginInit();
+            this.pnlConG.SuspendLayout();
+            this.pnlConA.SuspendLayout();
             this.SuspendLayout();
             // 
             // rbtn1
@@ -47,6 +75,7 @@ namespace Proj_Frag_App
             this.rbtn1.TabIndex = 0;
             this.rbtn1.TabStop = true;
             this.rbtn1.UseVisualStyleBackColor = true;
+            this.rbtn1.CheckedChanged += new System.EventHandler(this.rbtn1_CheckedChanged);
             // 
             // rbtn2
             // 
@@ -57,6 +86,7 @@ namespace Proj_Frag_App
             this.rbtn2.TabIndex = 1;
             this.rbtn2.TabStop = true;
             this.rbtn2.UseVisualStyleBackColor = true;
+            this.rbtn2.CheckedChanged += new System.EventHandler(this.rbtn2_CheckedChanged);
             // 
             // rbtn3
             // 
@@ -67,6 +97,7 @@ namespace Proj_Frag_App
             this.rbtn3.TabIndex = 2;
             this.rbtn3.TabStop = true;
             this.rbtn3.UseVisualStyleBackColor = true;
+            this.rbtn3.CheckedChanged += new System.EventHandler(this.rbtn3_CheckedChanged);
             // 
             // rbtn4
             // 
@@ -77,6 +108,7 @@ namespace Proj_Frag_App
             this.rbtn4.TabIndex = 3;
             this.rbtn4.TabStop = true;
             this.rbtn4.UseVisualStyleBackColor = true;
+            this.rbtn4.CheckedChanged += new System.EventHandler(this.rbtn4_CheckedChanged);
             // 
             // btnUPDATE
             // 
@@ -98,29 +130,247 @@ namespace Proj_Frag_App
             this.btnREAD.UseVisualStyleBackColor = true;
             this.btnREAD.Click += new System.EventHandler(this.btnREAD_Click);
             // 
-            // panel1
+            // pnlCONSULTAS
             // 
-            this.panel1.Location = new System.Drawing.Point(30, 148);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(924, 293);
-            this.panel1.TabIndex = 6;
+            this.pnlCONSULTAS.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.pnlCONSULTAS.Controls.Add(this.pnlConA);
+            this.pnlCONSULTAS.Controls.Add(this.pnlConF);
+            this.pnlCONSULTAS.Controls.Add(this.pnlConE);
+            this.pnlCONSULTAS.Controls.Add(this.btnGO);
+            this.pnlCONSULTAS.Controls.Add(this.dataGV);
+            this.pnlCONSULTAS.Controls.Add(this.pnlConG);
+            this.pnlCONSULTAS.Location = new System.Drawing.Point(30, 148);
+            this.pnlCONSULTAS.Name = "pnlCONSULTAS";
+            this.pnlCONSULTAS.Size = new System.Drawing.Size(924, 293);
+            this.pnlCONSULTAS.TabIndex = 6;
             // 
-            // Form1
+            // pnlConF
+            // 
+            this.pnlConF.Controls.Add(this.txtCF2);
+            this.pnlConF.Controls.Add(this.lblCF2);
+            this.pnlConF.Controls.Add(this.txtCF1);
+            this.pnlConF.Controls.Add(this.lblCF1);
+            this.pnlConF.Location = new System.Drawing.Point(15, 17);
+            this.pnlConF.Name = "pnlConF";
+            this.pnlConF.Size = new System.Drawing.Size(797, 53);
+            this.pnlConF.TabIndex = 5;
+            // 
+            // txtCF1
+            // 
+            this.txtCF1.Location = new System.Drawing.Point(88, 18);
+            this.txtCF1.MaxLength = 5;
+            this.txtCF1.Name = "txtCF1";
+            this.txtCF1.Size = new System.Drawing.Size(100, 22);
+            this.txtCF1.TabIndex = 1;
+            // 
+            // lblCF1
+            // 
+            this.lblCF1.AutoSize = true;
+            this.lblCF1.Location = new System.Drawing.Point(23, 18);
+            this.lblCF1.Name = "lblCF1";
+            this.lblCF1.Size = new System.Drawing.Size(59, 17);
+            this.lblCF1.TabIndex = 0;
+            this.lblCF1.Text = "Method:";
+            // 
+            // pnlConE
+            // 
+            this.pnlConE.Controls.Add(this.txtCE3);
+            this.pnlConE.Controls.Add(this.txtCE2);
+            this.pnlConE.Controls.Add(this.lblCE3);
+            this.pnlConE.Controls.Add(this.lblCE2);
+            this.pnlConE.Controls.Add(this.txtCE1);
+            this.pnlConE.Controls.Add(this.lblCE1);
+            this.pnlConE.Location = new System.Drawing.Point(15, 17);
+            this.pnlConE.Name = "pnlConE";
+            this.pnlConE.Size = new System.Drawing.Size(797, 53);
+            this.pnlConE.TabIndex = 4;
+            // 
+            // lblCE2
+            // 
+            this.lblCE2.AutoSize = true;
+            this.lblCE2.Location = new System.Drawing.Point(230, 18);
+            this.lblCE2.Name = "lblCE2";
+            this.lblCE2.Size = new System.Drawing.Size(60, 17);
+            this.lblCE2.TabIndex = 2;
+            this.lblCE2.Text = "SalesID:";
+            // 
+            // txtCE1
+            // 
+            this.txtCE1.Location = new System.Drawing.Point(104, 18);
+            this.txtCE1.MaxLength = 5;
+            this.txtCE1.Name = "txtCE1";
+            this.txtCE1.Size = new System.Drawing.Size(100, 22);
+            this.txtCE1.TabIndex = 1;
+            // 
+            // lblCE1
+            // 
+            this.lblCE1.AutoSize = true;
+            this.lblCE1.Location = new System.Drawing.Point(17, 18);
+            this.lblCE1.Name = "lblCE1";
+            this.lblCE1.Size = new System.Drawing.Size(65, 17);
+            this.lblCE1.TabIndex = 0;
+            this.lblCE1.Text = "Quantity:";
+            // 
+            // btnGO
+            // 
+            this.btnGO.Location = new System.Drawing.Point(825, 17);
+            this.btnGO.Name = "btnGO";
+            this.btnGO.Size = new System.Drawing.Size(75, 53);
+            this.btnGO.TabIndex = 2;
+            this.btnGO.Text = "GO";
+            this.btnGO.UseVisualStyleBackColor = true;
+            this.btnGO.Click += new System.EventHandler(this.btnGO_Click);
+            // 
+            // dataGV
+            // 
+            this.dataGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGV.Location = new System.Drawing.Point(15, 86);
+            this.dataGV.Name = "dataGV";
+            this.dataGV.RowHeadersWidth = 51;
+            this.dataGV.RowTemplate.Height = 24;
+            this.dataGV.Size = new System.Drawing.Size(885, 184);
+            this.dataGV.TabIndex = 1;
+            // 
+            // pnlConG
+            // 
+            this.pnlConG.Controls.Add(this.txtCG2);
+            this.pnlConG.Controls.Add(this.lblCG2);
+            this.pnlConG.Controls.Add(this.txtCG1);
+            this.pnlConG.Controls.Add(this.lblCG1);
+            this.pnlConG.Location = new System.Drawing.Point(15, 17);
+            this.pnlConG.Name = "pnlConG";
+            this.pnlConG.Size = new System.Drawing.Size(797, 53);
+            this.pnlConG.TabIndex = 0;
+            // 
+            // txtCG2
+            // 
+            this.txtCG2.Location = new System.Drawing.Point(311, 18);
+            this.txtCG2.MaxLength = 50;
+            this.txtCG2.Name = "txtCG2";
+            this.txtCG2.Size = new System.Drawing.Size(213, 22);
+            this.txtCG2.TabIndex = 3;
+            // 
+            // lblCG2
+            // 
+            this.lblCG2.AutoSize = true;
+            this.lblCG2.Location = new System.Drawing.Point(230, 18);
+            this.lblCG2.Name = "lblCG2";
+            this.lblCG2.Size = new System.Drawing.Size(75, 17);
+            this.lblCG2.TabIndex = 2;
+            this.lblCG2.Text = "new EMail:";
+            // 
+            // txtCG1
+            // 
+            this.txtCG1.Location = new System.Drawing.Point(104, 18);
+            this.txtCG1.MaxLength = 5;
+            this.txtCG1.Name = "txtCG1";
+            this.txtCG1.Size = new System.Drawing.Size(100, 22);
+            this.txtCG1.TabIndex = 1;
+            // 
+            // lblCG1
+            // 
+            this.lblCG1.AutoSize = true;
+            this.lblCG1.Location = new System.Drawing.Point(17, 18);
+            this.lblCG1.Name = "lblCG1";
+            this.lblCG1.Size = new System.Drawing.Size(85, 17);
+            this.lblCG1.TabIndex = 0;
+            this.lblCG1.Text = "CostumerID:";
+            // 
+            // lblCE3
+            // 
+            this.lblCE3.AutoSize = true;
+            this.lblCE3.Location = new System.Drawing.Point(425, 18);
+            this.lblCE3.Name = "lblCE3";
+            this.lblCE3.Size = new System.Drawing.Size(74, 17);
+            this.lblCE3.TabIndex = 4;
+            this.lblCE3.Text = "ProductID:";
+            // 
+            // txtCE2
+            // 
+            this.txtCE2.Location = new System.Drawing.Point(296, 18);
+            this.txtCE2.MaxLength = 5;
+            this.txtCE2.Name = "txtCE2";
+            this.txtCE2.Size = new System.Drawing.Size(100, 22);
+            this.txtCE2.TabIndex = 5;
+            // 
+            // txtCE3
+            // 
+            this.txtCE3.Location = new System.Drawing.Point(505, 18);
+            this.txtCE3.MaxLength = 5;
+            this.txtCE3.Name = "txtCE3";
+            this.txtCE3.Size = new System.Drawing.Size(100, 22);
+            this.txtCE3.TabIndex = 6;
+            // 
+            // txtCF2
+            // 
+            this.txtCF2.Location = new System.Drawing.Point(296, 18);
+            this.txtCF2.MaxLength = 5;
+            this.txtCF2.Name = "txtCF2";
+            this.txtCF2.Size = new System.Drawing.Size(100, 22);
+            this.txtCF2.TabIndex = 8;
+            // 
+            // lblCF2
+            // 
+            this.lblCF2.AutoSize = true;
+            this.lblCF2.Location = new System.Drawing.Point(230, 18);
+            this.lblCF2.Name = "lblCF2";
+            this.lblCF2.Size = new System.Drawing.Size(60, 17);
+            this.lblCF2.TabIndex = 7;
+            this.lblCF2.Text = "SalesID:";
+            // 
+            // pnlConA
+            // 
+            this.pnlConA.Controls.Add(this.txtCA1);
+            this.pnlConA.Controls.Add(this.lblCA1);
+            this.pnlConA.Location = new System.Drawing.Point(15, 17);
+            this.pnlConA.Name = "pnlConA";
+            this.pnlConA.Size = new System.Drawing.Size(797, 53);
+            this.pnlConA.TabIndex = 9;
+            // 
+            // txtCA1
+            // 
+            this.txtCA1.Location = new System.Drawing.Point(88, 18);
+            this.txtCA1.MaxLength = 5;
+            this.txtCA1.Name = "txtCA1";
+            this.txtCA1.Size = new System.Drawing.Size(100, 22);
+            this.txtCA1.TabIndex = 1;
+            // 
+            // lblCA1
+            // 
+            this.lblCA1.AutoSize = true;
+            this.lblCA1.Location = new System.Drawing.Point(23, 18);
+            this.lblCA1.Name = "lblCA1";
+            this.lblCA1.Size = new System.Drawing.Size(59, 17);
+            this.lblCA1.TabIndex = 0;
+            this.lblCA1.Text = "Method:";
+            // 
+            // frmPRINCIPAL
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(982, 453);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlCONSULTAS);
             this.Controls.Add(this.btnREAD);
             this.Controls.Add(this.btnUPDATE);
             this.Controls.Add(this.rbtn4);
             this.Controls.Add(this.rbtn3);
             this.Controls.Add(this.rbtn2);
             this.Controls.Add(this.rbtn1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Name = "Form1";
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "frmPRINCIPAL";
             this.Text = "App Consultas Distribuidas";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlCONSULTAS.ResumeLayout(false);
+            this.pnlConF.ResumeLayout(false);
+            this.pnlConF.PerformLayout();
+            this.pnlConE.ResumeLayout(false);
+            this.pnlConE.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGV)).EndInit();
+            this.pnlConG.ResumeLayout(false);
+            this.pnlConG.PerformLayout();
+            this.pnlConA.ResumeLayout(false);
+            this.pnlConA.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,7 +384,29 @@ namespace Proj_Frag_App
         private System.Windows.Forms.RadioButton rbtn4;
         private System.Windows.Forms.Button btnUPDATE;
         private System.Windows.Forms.Button btnREAD;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel pnlCONSULTAS;
+        private System.Windows.Forms.Button btnGO;
+        private System.Windows.Forms.DataGridView dataGV;
+        private System.Windows.Forms.Panel pnlConG;
+        private System.Windows.Forms.TextBox txtCG2;
+        private System.Windows.Forms.Label lblCG2;
+        private System.Windows.Forms.TextBox txtCG1;
+        private System.Windows.Forms.Label lblCG1;
+        private System.Windows.Forms.Panel pnlConF;
+        private System.Windows.Forms.TextBox txtCF1;
+        private System.Windows.Forms.Label lblCF1;
+        private System.Windows.Forms.Panel pnlConE;
+        private System.Windows.Forms.Label lblCE2;
+        private System.Windows.Forms.TextBox txtCE1;
+        private System.Windows.Forms.Label lblCE1;
+        private System.Windows.Forms.TextBox txtCE3;
+        private System.Windows.Forms.TextBox txtCE2;
+        private System.Windows.Forms.Label lblCE3;
+        private System.Windows.Forms.TextBox txtCF2;
+        private System.Windows.Forms.Label lblCF2;
+        private System.Windows.Forms.Panel pnlConA;
+        private System.Windows.Forms.TextBox txtCA1;
+        private System.Windows.Forms.Label lblCA1;
     }
 }
 
--- Proj_Frag_App/Form1.cs
+++ Proj_Frag_App/Form1.cs
@@ -7,19 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Proj_Frag_App
 {
-    public partial class Form1 : Form
+    public partial class frmPRINCIPAL : Form
     {
-        public Form1()
+        int consulta = 0;
+        int tipo = 0;
+        SqlConnection conn;
+
+        public frmPRINCIPAL()
         {
             InitializeComponent();
+            conn = Conexion.conectaSQL();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             rbtn1.Visible = rbtn2.Visible = rbtn3.Visible = rbtn4.Visible = false;
+            pnlConA.Visible = pnlConE.Visible = pnlConF.Visible = pnlConG.Visible = false;
+            btnGO.Enabled = false;
         }
 
         private void btnUPDATE_Click(object sender, EventArgs e)
@@ -29,6 +37,7 @@ namespace Proj_Frag_App
             rbtn3.Text = "Actualizar el método de envío de una orden.";
             rbtn4.Text = "Actualizar el correo electrónico de una cliente.";
             rbtn1.Visible = rbtn2.Visible = rbtn3.Visible = rbtn4.Visible = true;
+            tipo = 1; 
         }
 
         private void btnREAD_Click(object sender, EventArgs e)
@@ -38,6 +47,190 @@ namespace Proj_Frag_App
             rbtn3.Text = "Determinar si hay clientes que realizan ordenes en territorios diferentes al que se encuentran.";
             rbtn1.Visible = rbtn2.Visible = rbtn3.Visible = true;
             rbtn4.Visible = false;
+            tipo = 2;
         }
+
+        private void rbtn1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbtn1.Checked == true)
+            {
+                rbtn2.Checked = rbtn3.Checked = rbtn4.Checked = false;
+                consulta = 1;
+            }
+            pnl_Refresh();
+        }
+
+        private void rbtn2_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbtn2.Checked == true)
+            {
+                rbtn1.Checked = rbtn3.Checked = rbtn4.Checked = false;
+                consulta = 2;
+            }
+            pnl_Refresh();
+        }
+
+        private void rbtn3_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbtn3.Checked == true)
+            {
+                rbtn2.Checked = rbtn1.Checked = rbtn4.Checked = false;
+                consulta = 3;
+            }
+            pnl_Refresh();
+        }
+
+        private void rbtn4_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rbtn4.Checked == true)
+            {
+                rbtn2.Checked = rbtn3.Checked = rbtn1.Checked = false;
+                consulta = 4;
+            }
+            pnl_Refresh();
+        }
+
+        public void pnl_Refresh() 
+        {
+            pnlConA.Visible = pnlConE.Visible = pnlConF.Visible = pnlConG.Visible = false;
+
+            if (tipo != 0 && consulta !=0)
+            {
+                btnGO.Enabled = true;
+            }
+            else
+            {
+                btnGO.Enabled = false;
+            }
+
+            switch (tipo)
+            {
+                case 1:
+                    // UPDATE
+                    switch (consulta)
+                    {
+                        case 1:
+                            // CONSULTA
+                            break;
+                        case 2:
+                            // CONSULTA E
+                            pnlConE.Visible = true;
+                            break;
+                        case 3:
+                            // CONSULTA F
+                            pnlConF.Visible = true;
+                            break;
+                        case 4:
+                            // CONSULTA G
+                            pnlConG.Visible = true;
+                            break;
+                    }
+                    break;
+                case 2:
+                    // READ
+                    switch (consulta)
+                    {
+                        case 1:
+                            // CONSULTA A
+                            pnlConA.Visible = true;
+                            break;
+                        case 2:
+                            // CONSULTA
+                            break;
+                        case 3:
+                            // CONSULTA
+                            break;
+                    }
+                    break;
+            }
+        }
+
+        private void btnGO_Click(object sender, EventArgs e)
+        {
+            SqlCommand com = new SqlCommand("", conn);
+            int resultado = 0;
+
+            try
+            {
+                switch (tipo)
+                {
+                    case 1:
+                        // UPDATE
+                        switch (consulta)
+                        {
+                            case 1:
+                                // CONSULTA
+                                break;
+                            case 2:
+                                // CONSULTA E
+                                com.CommandText = "ce_updateSales";
+                                com.CommandType = CommandType.StoredProcedure;
+                                com.Parameters.AddWithValue("@qty", txtCE1.Text).Direction = ParameterDirection.Input;
+                                com.Parameters.AddWithValue("@salesID", txtCE2.Text).Direction = ParameterDirection.Input;
+                                com.Parameters.AddWithValue("@productID", txtCE3.Text).Direction = ParameterDirection.Input;
+                                conn.Open();
+                                resultado = com.ExecuteNonQuery();
+                                conn.Close();
+                                break;
+                            case 3:
+                                // CONSULTA F
+                                com.CommandText = "cf_updateShip"; 
+                                com.CommandType = CommandType.StoredProcedure;
+                                com.Parameters.AddWithValue("@method", txtCF1.Text).Direction = ParameterDirection.Input;
+                                com.Parameters.AddWithValue("@salesID", txtCF2.Text).Direction = ParameterDirection.Input;
+                                conn.Open();
+                                resultado = com.ExecuteNonQuery();
+                                conn.Close();
+                                break;
+                            case 4:
+                                // CONSULTA G
+                                com.CommandText = "cg_updateEmail";
+                                com.CommandType = CommandType.StoredProcedure;
+                                com.Parameters.AddWithValue("@customerID", txtCG1.Text).Direction = ParameterDirection.Input;
+                                com.Parameters.AddWithValue("@newEmail", txtCG2.Text).Direction = ParameterDirection.Input;
+                                conn.Open();
+                                resultado = com.ExecuteNonQuery();
+                                conn.Close();
+                                break;
+                        }
+                        break;
+                    case 2:
+                        // READ
+                        switch (consulta)
+                        {
+                            case 1:
+                                // CONSULTA A
+                                com.CommandText = "ca_selectTotalProd";
+                                com.CommandType = CommandType.StoredProcedure;
+                                com.Parameters.AddWithValue("@cat", txtCA1.Text).Direction = ParameterDirection.Input;
+                                conn.Open();
+                                resultado = com.ExecuteNonQuery();
+                                conn.Close();
+                                break;
+                            case 2:
+                                // CONSULTA
+                                break;
+                            case 3:
+                                // CONSULTA
+                                break;
+                        }
+                        break;
+                }
+
+                if (resultado > 0)
+                {
+                    MessageBox.Show("Consulta Ejecutada Correctamente", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Ocurrio un Error en la Ejecución", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
     }
 }
